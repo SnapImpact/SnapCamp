aa5aef0aa1c4a1ba622665dc47c90b01cd2fc428
koeninger <cody@koeninger.org>
2010-02-20 19:37:37 +0000

    query parameter to set output type rss/json, needs to change doctype

--- src/main/scala/org/snapimpact/view/api.scala
+++ src/main/scala/org/snapimpact/view/api.scala
@@ -8,7 +8,6 @@ class api extends LiftView {
   override def dispatch = { 
     case "index" => index _
     case "volopps" => volopps _
-
   }
   
   def index(): NodeSeq = { 
@@ -16,10 +15,24 @@ class api extends LiftView {
   }
 
   def volopps(): NodeSeq = S.param("key") match {
-    case Full(x) =>
-        <p>here's some info on volunteer opportunities</p>
-    case _ =>
-      <p>You seem to be missing the API key parameter ('key') in your query.  Please see the <a href="http://www.allforgood.org/docs/api.html">directions for how to get an API key</a>.</p>
+    case Full(x) => S.param("output") match { 
+      case Full("json") => sampleJson
+      case Full("rss") => sampleRss
+      case _ => sampleHtml
+    }
+    case _ => missingKey
   }
+
+  val missingKey =
+    <p>You seem to be missing the API key parameter ('key') in your query.  Please see the <a href="http://www.allforgood.org/docs/api.html">directions for how to get an API key</a>.</p>
+
+  val sampleHtml =
+    <p>here's some info on volunteer opportunities</p>
+
+  val sampleJson =
+    <p>here's some json with the wrong doctype</p>
+
+  val sampleRss =
+    <p>here's some rss with the wrong doctype</p>
 }
 
