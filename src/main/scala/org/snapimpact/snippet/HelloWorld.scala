b8135a18ebc03293d458c77418f03b5bf72834d8
Zachary Kim <zkim@napple.local>
2010-02-20 15:23:45 -0700

    Got jpa stuff working for Timeframe jpa class, next step is moving the rest of the existing JPA stuff over

--- src/main/scala/org/snapimpact/snippet/HelloWorld.scala
+++ src/main/scala/org/snapimpact/snippet/HelloWorld.scala
@@ -4,21 +4,27 @@ package snippet {
 import _root_.scala.xml.{NodeSeq, Text}
 import _root_.net.liftweb.util._
 import _root_.net.liftweb.common._
-import _root_.java.util.Date
 import org.snapimpact.lib._
 import Helpers._
+import model.{Timeframe, Model}
+import java.util.{UUID, Date}
+import java.math.BigInteger
 
 class HelloWorld {
-  lazy val date: Box[Date] = DependencyFactory.inject[Date] // inject the date
+    lazy val date: Box[Date] = DependencyFactory.inject[Date] // inject the date
 
-  def howdy(in: NodeSeq): NodeSeq =
-  Helpers.bind("b", in, "time" -> date.map(d => Text(d.toString)))
+    def howdy(in: NodeSeq): NodeSeq = {
+        val tf = new Timeframe(UUID.randomUUID.toString, UUID.randomUUID.toString, new BigInteger("10"))
+        Model.persist(tf)
+        Helpers.bind("b", in, "time" -> date.map(d => Text(d.toString)))
+    }
 
-  /*
-   lazy val date: Date = DependencyFactory.time.vend // create the date via factory
 
-   def howdy(in: NodeSeq): NodeSeq = Helpers.bind("b", in, "time" -> date.toString)
-   */
+    /*
+    lazy val date: Date = DependencyFactory.time.vend // create the date via factory
+
+    def howdy(in: NodeSeq): NodeSeq = Helpers.bind("b", in, "time" -> date.toString)
+    */
 }
 
 }
