90ea1ae76ca52fb66cc9fcfb6b5c1b0eb4fc995c
koeninger <cody@koeninger.org>
2010-02-20 18:39:30 +0000

    check that key exists, which is all the current api does for security

--- src/main/scala/org/snapimpact/view/api.scala
+++ src/main/scala/org/snapimpact/view/api.scala
@@ -1,7 +1,8 @@
 package org.snapimpact.view
 
 import scala.xml.NodeSeq
-import net.liftweb.http.LiftView
+import net.liftweb.http.{S, LiftView}
+import net.liftweb.common.{Box, Empty, Full}
 
 class api extends LiftView { 
   override def dispatch = { 
@@ -14,8 +15,11 @@ class api extends LiftView {
     <p>here's some info about the api</p>
   }
 
-  def volopps(): NodeSeq = {
-    <p>here's some info on volunteer opportunities</p>
+  def volopps(): NodeSeq = S.param("key") match {
+    case Full(x) =>
+        <p>here's some info on volunteer opportunities</p>
+    case _ =>
+      <p>You seem to be missing the API key parameter ('key') in your query.  Please see the <a href="http://www.allforgood.org/docs/api.html">directions for how to get an API key</a>.</p>
   }
 }
 
