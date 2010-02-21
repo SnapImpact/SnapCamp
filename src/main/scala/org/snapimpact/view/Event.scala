e6c318686a653132d0a80a17f6ab65507e63f85a
unknown <okristjansson@.us.deloitte.com>
2010-02-20 19:56:05 -0700

    case classes that can be used for the rest api

---
+++ src/main/scala/org/snapimpact/view/Event.scala
@@ -0,0 +1,17 @@
+package org.snapimpact.view
+{
+import org.scala_tools.time.Imports._
+
+case class Interest(uri: String)
+case class Location(uri: String)
+case class Organization(uri: String, description: String, id: String, name: String, organizationTypeId: String)
+case class Source(etlClass: String, id: String, name: String, url: String)
+case class TimeStamp(uri: String, id: String, timestamp: DateTime )
+                                                                      
+case class Events(uri: String, sourceName: String, description: String, duration: String, email: String,
+        id: String, interestAreaCollection: List[Interest], LocationCollection: List[Location],
+        organizationCollection: List[Organization], phone: String, sourceId: String,
+        sourceUrl: String, timestampCollection: List[TimeStamp], title: String,
+        url: String)
+
+}
