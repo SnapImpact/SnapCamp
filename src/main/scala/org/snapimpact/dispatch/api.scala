f8d2083b584d00da1d00ca04f2a674a156b597ac
koeninger <cody@koeninger.org>
2010-02-21 00:33:46 +0000

    remove view based api custom dispatch api needs to return xml encoding in the prolog, but its seems like more pain than its worth right now

--- src/main/scala/org/snapimpact/dispatch/api.scala
+++ src/main/scala/org/snapimpact/dispatch/api.scala
@@ -9,10 +9,11 @@ object api {
   def volopps(): Box[LiftResponse] = Full( S.param("key") match { 
     case Full(_) => S.param("output") match {
       case Full("json") => JsonResponse(sampleJson, ("Content-Type" -> "text/javascript") :: Nil, Nil, 200)
-      case Full("rss") => XmlResponse(sampleRss, 200, "application/rss+xml", Nil)
-      case _ => XmlResponse(sampleHtml, 200, "application/xhtml+xml; charset=utf-8", Nil)
+      //XXX these responses should include encoding in the xml prolog
+      case Full("rss") => XmlResponse(sampleRss, "application/rss+xml")
+      case _ => XmlResponse(sampleHtml, "application/xhtml+xml; charset=utf-8")
     }
-    case _ => XmlResponse(missingKey, 401, "application/xhtml+xml; charset=utf-8", Nil)
+    case _ => XmlResponse(missingKey, 401, "application/xhtml+xml; charset=utf-8")
   } )
 
   val missingKey =
@@ -24,6 +25,5 @@ object api {
   val sampleJson =
     JsRaw("""{"here": "is some sample json"}""")
 
-  val sampleRss =
-    <p>here's some rss with the wrong doctype</p>
+  val sampleRss = <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom" xmlns:fp="http://www.allforgood.org/" xmlns:georss="http://www.georss.org/georss" xmlns:gml="http://www.opengis.net/gml"><p>here's some rss with the wrong doctype</p></rss>
 }
