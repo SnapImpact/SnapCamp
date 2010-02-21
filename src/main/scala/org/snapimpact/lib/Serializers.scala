71233c89cc5b6247efa8ff1e2a909d404db029a0
koeninger <cody@koeninger.org>
2010-02-21 19:12:57 +0000

    handle collections, collections of primitives don't have separator between elements

--- src/main/scala/org/snapimpact/lib/Serializers.scala
+++ src/main/scala/org/snapimpact/lib/Serializers.scala
@@ -3,15 +3,34 @@ package org.snapimpact.lib
 import scala.xml.{NodeSeq, Elem, Null, TopScope, Text}
 
 object Serializers {
+  val ns = "fp"
+
+  // primitives stolen from lift-json
+  val primitives = Map[Class[_], Unit]() ++ (List[Class[_]](
+    classOf[String], classOf[Int], classOf[Long], classOf[Double], 
+    classOf[Float], classOf[Byte], classOf[BigInt], classOf[Boolean], 
+    classOf[Short], classOf[java.lang.Integer], classOf[java.lang.Long], 
+    classOf[java.lang.Double], classOf[java.lang.Float], 
+    classOf[java.lang.Byte], classOf[java.lang.Boolean], classOf[Number],
+    classOf[java.lang.Short], classOf[java.util.Date], classOf[Symbol]).map((_, ())))
+
+  /**
+   * serialize anything to allforgood fp rss
+   */
+  def anyToRss(a: AnyRef): NodeSeq = a match { 
+    case p if primitives contains p.getClass => Text(p.toString)
+    case s: Seq[AnyRef]  => s.flatMap(x => anyToRss(x))
+    case _ => ccToRss(a)
+  } 
+
   /**
    * serialize a case class to allforgood fp rss
    */
   def ccToRss(cc: AnyRef): NodeSeq = { 
-    val ns = "fp"
     val fields = cc.getClass.getDeclaredFields
     fields.map( f => { 
       f.setAccessible(true)
-      Elem(ns, f.getName, Null, TopScope, Text(f.get(cc).toString))
+      Elem(ns, f.getName, Null, TopScope, anyToRss(f.get(cc)): _*)
     })
   }
   
