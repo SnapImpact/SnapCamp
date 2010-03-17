fb32bbc38be3c34b9373a034e65a4ad3697700ca
mchance <mark@peopledesigned.com>
2010-03-16 19:56:35 -0600

    add a file upload page

---
+++ src/main/scala/org/snapimpact/snippet/XmlUploadSnippet.scala
@@ -0,0 +1,11 @@
+package org.snapimpact.snippet
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: mark
+ * Date: Mar 16, 2010
+ * Time: 7:36:23 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+class XmlUploadSnippet
