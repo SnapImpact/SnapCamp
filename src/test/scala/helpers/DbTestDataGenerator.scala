435babef929e9af28ca098346daeb17877788583
Dave Angulo <daveangulo@snapimpact.org>
2010-04-20 15:43:14 -0600

    Removed all of the deprecated java jpa classes and tests. Cleaned up the remnants.

--- src/test/scala/helpers/DbTestDataGenerator.scala
+++ src/test/scala/helpers/DbTestDataGenerator.scala
@@ -1,7 +1,6 @@
 package helpers
 
 import _root_.java.util.{ArrayList, UUID}
-import org.snapimpact.model.{InterestArea, Event}
 import org.snapimpact.etl.model.dto.FootprintFeed
 import xml.XML
 
@@ -14,34 +13,6 @@ import xml.XML
  */
 
 object DbTestDataGenerator {
-    def genEvent = {
-        val e = new Event
-        e.setId(rndStr)
-        e.setTitle(rndStr)
-        e.setDescription(rndStr)
-        e.setDuration(rndLong)
-        e.setContact(rndStr)
-        e.setUrl(rndStr)
-        e.setPhone(rndStr)
-        e.setEmail(rndStr)
-        e.setSourceKey(rndStr)
-        e.setSourceUrl(rndStr)
-        e.setInterestAreaCollection(new ArrayList[InterestArea]())
-        e.getInterestAreaCollection.add(genInterestArea)
-        e.getInterestAreaCollection.add(genInterestArea)
-        e.getInterestAreaCollection.add(genInterestArea)
-
-        e
-    }
-
-    def genInterestArea = {
-        val i = new InterestArea
-        i.setId(rndStr)
-        i.setName(rndStr)
-
-        i
-    }
-
     def rndStr = UUID.randomUUID.toString
     def rndLong = (Math.random * 1000).asInstanceOf[Long]
 
