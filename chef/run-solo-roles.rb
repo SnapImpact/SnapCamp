08ce5578456fe4aaf810ce0c068eb6c69af19e8b
ryanschneider <ryanleeschneider@gmail.com>
2010-05-01 20:33:36 -0600

    My first ruby code review from @DrawOhara :)

--- chef/run-solo-roles.rb
+++ chef/run-solo-roles.rb
@@ -1,20 +1,15 @@
 #!/usr/bin/env ruby
 
-#run a system command
-def cmd(cmd)
-  system(cmd)
-end
-
 require "rubygems"
 gem "json"
 require "json"
 
 puts( "Generating run_list..." )
-roles = ARGV.collect { |x| "role[" << x << "]" }
+roles = ARGV.map{|x| "role[" << x << "]" }
 json = { "run_list" => roles }
-File.open('/tmp/dna.json', 'w') { |f| f.write(JSON.generate(json)) }
+open('/tmp/dna.json', 'w') { |f| f.write(JSON.generate(json)) }
 puts( "Starting chef-solo...")
-cmd( "chef-solo -c /tmp/bootstrap/chef/solo.rb -j /tmp/dna.json"  )
+system( "chef-solo -c /tmp/bootstrap/chef/solo.rb -j /tmp/dna.json"  )
 
 
 
