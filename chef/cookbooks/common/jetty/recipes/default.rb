73d092fbb6f2162fe89931774fe005231f8afc4d
ryanschneider <ryanleeschneider@gmail.com>
2010-05-02 15:28:13 -0600

    Updated jetty recipe to be inline with what I'm seeing on Ubuntu. Needed to specify ::server for postgresql on DBMASTER role.

--- chef/cookbooks/common/jetty/recipes/default.rb
+++ chef/cookbooks/common/jetty/recipes/default.rb
@@ -1,30 +1,16 @@
 include_recipe "java"
 # Needed by jetty extra & jsp
-%w{ ant libgnujaf-java libgnumail-java }.each do |pkg|
+%w{ libjetty6-java libjetty6-extra-java jetty }.each do |pkg|
  package pkg do
- action :install
+  action :install
  end
-end
-
-jetty_version = "6.1.16_all"
-jetty_debs = %w{libjetty6-java libjetty6-jsp-java libjetty6-extra-java jetty6}
-
-jetty_debs.each do |deb|
- remote_file deb do
- path "/tmp/#{deb}_#{jetty_version}.deb"
- source "http://dist.codehaus.org/jetty/jetty-6.1.16/debs/#{deb}_#{jetty_version}.deb"
- end
-end
-
-bash "install-jetty" do
- code "cd /tmp && dpkg --install *jetty6*.deb"
 end
 
 bash "enable-jetty" do
- code "sed s/NO_START=1/NO_START=0/ -i /etc/default/jetty6"
+ code "sed s/NO_START=1/NO_START=0/ -i /etc/default/jetty"
 end
 
-service "jetty6" do
+service "jetty" do
  supports :restart => true, :reload => true
  action [ :enable, :start ]
 end
