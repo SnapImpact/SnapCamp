35c97d3956f57b43b75caf4690a08a3212890693
ryanschneider <ryanleeschneider@gmail.com>
2010-05-01 20:10:29 -0600

    moved tarball url to a var

--- clean-boot/alestic-allroles.sh
+++ clean-boot/alestic-allroles.sh
@@ -2,6 +2,7 @@
 ## For Alestic AMIs (I'm using ami-6743ae0e)
 ## What are our roles?
 export ROLES=( WEB APP DBMASTER )
+export BOOTSTRAP_TARBALL_URL='http://github.com/ryanschneider/afg-bootstrap/tarball/master' #TODO: use S3
 ## log all commands, and exit on any errors (to aid debugging)
 ## all stdout/stderr will be piped to /var/log/syslog
 set -e -x
@@ -13,7 +14,7 @@ apt-get -y update
 cd /tmp
 mkdir bootstrap
 cd bootstrap
-curl -L 'http://github.com/ryanschneider/afg-bootstrap/tarball/master' | tar xvzf -
+curl -L $BOOTSTRAP_TARBALL_URL | tar xvzf -
 ## tarfile may have our stuff at root, or in a subdir
 ## if chef isn't here, then (hopefully) everything is in a single subdir
 if [ ! -d  chef ];
