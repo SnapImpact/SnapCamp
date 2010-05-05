20e02b6ed4b021669bb390f88c09e99d66716083
ryanschneider <ryanleeschneider@gmail.com>
2010-05-04 20:59:16 -0600

    hopefully installing the pg_hba.conf file

---
+++ chef/cookbooks/allforgood/afg_dbmaster/recipes/default.rb
@@ -0,0 +1,22 @@
+include_recipe "postgresql::server"
+
+#hopefully all we need to do is setup local access.
+#the lift app will actually be the one to create the DB/tables.
+
+#stop postgres
+service "postgres-8.3" do
+  action :stop
+end
+
+#copy the pg_hba.conf to the proper location
+remote_file "pg_hba_conf" do
+    source "pg_hba.conf"
+    path "/etc/postgresql/8.3/main/pg_hba.conf"
+    mode 0600
+    owner "postgres"
+end
+
+#and restart the service
+service "postgres-8.3" do
+  action :start
+end
